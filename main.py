f03565299fb9d79544d0ee066c1160cc59389771
misterabs26 <mister.abs26@gmail.com>
2025-04-21 21:12:36 +0800

    Implement a simple datepicker to retrieve data from the API

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import streamlit as st
 import requests
+from datetime import datetime
+
+from streamlit import divider
 
 st.title("Astronomy Photo of the Day")
+st.divider()
 
-api_key = st.secrets["api"]["API_KEY"]
-url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+# Date input
+selected_date = st.date_input("Choose a date",
+                              min_value=None,
+                              max_value=datetime.today())
 
-response = requests.get(url).json()
+if selected_date:
+    formatted_date = selected_date.strftime("%Y-%m-%d")
 
-st.header(response["title"])
-if response["media_type"] == "image":
-    if response["hdurl"]:
-        st.image(response["hdurl"], use_container_width=True)
-    else:
-        st.image(response["url"], use_container_width=True)
-st.text(response["explanation"])
+    api_key = st.secrets["api"]["API_KEY"]
+    url = (f"https://api.nasa.gov/planetary/apod?api_key={api_key}&"
+           f"date={formatted_date}")
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        st.header(data["title"],divider="rainbow")
+
+        if data["media_type"] == "image":
+            # when hd quality is available
+            if data["hdurl"]:
+                st.image(data["hdurl"], use_container_width=True)
+            else:
+                st.image(data["url"], use_container_width=True)
+        else:
+            st.video(data["url"])
+
+        st.write(data["explanation"])
